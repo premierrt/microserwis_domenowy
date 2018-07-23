3d4aafb4b2047954972d9ee145b7ec221b31de35
Rafał <rafal@xxx.xx>
2018-07-23 14:24:07 +0200

    init

---
+++ src/main/java/microservice_domenowy/MicroserviceDomenowyApplication.java
@@ -0,0 +1,12 @@
+package microservice_domenowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDomenowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDomenowyApplication.class, args);
+	}
+}
